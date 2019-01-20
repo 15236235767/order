cc0ed35382dcb6aeb473943f1fea1a6ef178de02
lanle <lanle@zzl.com>
2019-01-20 20:30:07 +0800

    修改shell脚本

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 #编译+部署order站点
 #需要配置如下参数
-# 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
-# export PROJ_PATH=这个jenkins任务在部署机器上的路径
 # 输入你的环境上tomcat的全路径
 # export TOMCAT_APP_PATH=tomcat在部署机器上的路径
 ### base 函数
@@ -16,10 +14,9 @@ killTomcat(){
       kill -9 $pid
     fi
 }
-# cd $PROJ_PATH/order
-# mvn clean install
 # 停tomcat
-killTomcat
+cd /home/admin/apache-tomcat-8.5.31/bin/
+sh shutdown.sh
 # 删除原有工程
 rm -rf /home/admin/apache-tomcat-8.5.31/webapps/order
 rm -f /home/admin/apache-tomcat-8.5.31/webapps/order.war
