3acceb2942a7c9e51c7b74a0bab02444a3d854d7
lanle <lanle@zzl.com>
2019-01-20 20:31:51 +0800

    修改shell脚本

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,6 @@
 #需要配置如下参数
 # 输入你的环境上tomcat的全路径
 # export TOMCAT_APP_PATH=tomcat在部署机器上的路径
-### base 函数
-killTomcat(){
-    pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
-    echo "tomcat Id list :$pid"
-    if [ "$pid" = "" ]
-    then
-      echo "no tomcat pid alive"
-    else
-      kill -9 $pid
-    fi
-}
 # 停tomcat
 cd /home/admin/apache-tomcat-8.5.31/bin/
 sh shutdown.sh
@@ -26,6 +15,3 @@ cp /home/admin/app/order.war /home/admin/apache-tomcat-8.5.31/webapps/
 # 启动Tomcat
 cd /home/admin/apache-tomcat-8.5.31/bin/
 sh startup.sh
-
-
-
